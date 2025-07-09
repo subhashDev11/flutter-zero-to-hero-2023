c3d547b5316510bb9d5812835151bc61d10fb698
Subhash Chandra Shukla <70679949+subhashDev11@users.noreply.github.com>
2025-07-09 12:05:29 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_zero_to_hero_2023/main.dart';
+import 'package:flutter_zero_to_hero_2025/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
